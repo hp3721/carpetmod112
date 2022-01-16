1bb0afd5c1e1fd0948639fa874baffa8ff3216a7
BuildTools <44118666+hp3721@users.noreply.github.com>
2022-01-16 10:49:10 -0600

    1.12.0 Backport

--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -45,8 +45,8 @@ public class CarpetSettings
 
     // TODO: replace these constants at build time
     public static final String carpetVersion = "v21_11_10";
-    public static final String minecraftVersion = "1.12.2";
-    public static final String mcpMappings = "39-1.12";
+    public static final String minecraftVersion = "1.12";
+    public static final String mcpMappings = "20180814-1.12";
 
     public static final Logger LOG = LogManager.getLogger();
 
